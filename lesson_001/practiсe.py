6b1333260ecb37426096a3646ff93c89f2d97a35
denyunusov13-learning <den.yunusov13@gmail.com>
2026-06-07 18:34:24 +0700

    добавил файл практики

---
+++ lesson_001/practiсe.py
@@ -0,0 +1,22 @@
+my_haigh = 174
+print(my_haigh)
+
+my_name = 'Den'
+my_name = 'Denis Yunusov'
+print(my_name)
+
+pet_name = input("Как зовут вашего питомца? ")
+print("Вашего питомца зовут - "+ pet_name)
+
+def print_python():
+    print("Учу пайтон!")
+print_python()
+
+def print_letter(let):
+    print(let)
+print_letter('S')
+print_letter('k')
+print_letter('y')
+print_letter('P')
+print_letter('r')
+print_letter('o')
