a27ec570d815128cfb27212ae4fdaea81a3b8442
denyunusov13-learning <den.yunusov13@gmail.com>
2026-06-07 19:01:51 +0700

    Скорректировал файлы домашнего задания

---
+++ lesson_001/lesson_1_task_5.py
@@ -0,0 +1,14 @@
+def print_tel(num):
+    print(num, end="")
+
+print_tel(8)
+print_tel(8)
+print_tel(0)
+print_tel(0)
+print_tel(5)
+print_tel(5)
+print_tel(5)
+print_tel(3)
+print_tel(5)
+print_tel(3)
+print_tel(5)
