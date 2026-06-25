5e6f6fae0fa1f0ffcf50b81457ac4a4adf5f7e61
denyunusov13-learning <den.yunusov13@gmail.com>
2026-06-25 23:19:24 +0700

    добавлены все необходимые файлы для начала работы

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -9,6 +9,8 @@ address_from = Address("633333", "Новосибирск",
 message = Mailing(address_to, address_from, 500, "152334")
 print(f"Отправление {message.track} из {address_from.index}, "
       f"{address_from.city}, {address_from.street}, {address_from.house} - "
-      f"{address_from.apartment} в {address_to.index}, {address_to.city}, {address_to.street},"
-      f" {address_to.house} - {address_to.apartment}. Стоимость {message.cost} рублей."
-)
+      f"{address_from.apartment} в {address_to.index}, {address_to.city}, "
+      f"{address_to.street},"
+      f" {address_to.house} - {address_to.apartment}. Стоимость "
+      f"{message.cost} рублей."
+      )
