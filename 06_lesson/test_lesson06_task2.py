8100354c5c8d90a03f2b11e6db41bb7ddd531437
denyunusov13-learning <den.yunusov13@gmail.com>
2026-07-07 21:40:15 +0700

    сам себя исправил 2 раз, окончательный вариант

--- 06_lesson/test_lesson06_task2.py
+++ 06_lesson/test_lesson06_task2.py
@@ -1,12 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_dynamic_loading():
     driver = webdriver.Chrome()
-    wait = WebDriverWait(driver, 10)
     driver.maximize_window()
     driver.get("https://gitflic.ru/")
 
