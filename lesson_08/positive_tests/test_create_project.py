0acbec39849b4c0d08c384e59c47a417fd26f068
denyunusov13-learning <den.yunusov13@gmail.com>
2026-08-06 18:18:01 +0700

    перенес фикстуры в системный confest.py, изменил put на get

--- lesson_08/positive_tests/test_create_project.py
+++ lesson_08/positive_tests/test_create_project.py
@@ -1,48 +1,7 @@
-import os
 import requests
 import pytest
-from dotenv import load_dotenv
 
 BASE_URL = "https://ru.yougile.com/api-v2"
-load_dotenv()
-
-
-@pytest.fixture(scope="session")
-def company_id():
-    """
-    Шаг 1: получаем company_id из переменных окружения
-    """
-    comp_id = os.getenv("YOUGILE_COMPANY_ID")
-    if not comp_id:
-        pytest.fail("Не задан YOUGILE_COMPANY_ID в .env!")
-    print(f"[INFO] Используем company_id: {comp_id}")
-    return comp_id
-
-
-@pytest.fixture
-def get_api_key():
-    """
-    Шаг 2: получаем API-ключ через логин/пароль
-    (я его позже убрал, так как тесты начали падать
-    с ошибкой 403... не понимал что происходит и тут,
-    прочитав документацию увидел, что максимум ключей -30,
-    а я создаю их каждый раз, поэтому использую статический)
-    """
-    key = os.getenv("YOUGILE_API_KEY")
-    if not key:
-        pytest.fail("Не задан YOUGILE_API_KEY в .env! Создай ключ")
-    return key
-
-
-@pytest.fixture
-def headers(get_api_key):
-    """
-    Шаг 3: определяем хэдэры для запроса
-    """
-    return {
-        "Authorization": f"Bearer {get_api_key}",
-        "Content-Type": "application/json"
-    }
 
 
 def test_create_project(headers, company_id):
--- lesson_08/positive_tests/test_get_project.py
+++ lesson_08/positive_tests/test_get_project.py
@@ -1,50 +1,14 @@
-import os
 import requests
 import pytest
-from dotenv import load_dotenv
 
 BASE_URL = "https://ru.yougile.com/api-v2"
-load_dotenv()
 
 
-@pytest.fixture
-def get_api_key():
-    """
-    Шаг 1: подтягиваем ключ из .env
-    """
-    key = os.getenv("YOUGILE_API_KEY")
-    if not key:
-        pytest.fail("Не задан YOUGILE_API_KEY в .env! Создай ключ")
-    return key
-
-
-@pytest.fixture
-def headers(get_api_key):
-    """
-    Шаг 2: определяем хэдэры для запроса
-    """
-    return {
-        "Authorization": f"Bearer {get_api_key}",
-        "Content-Type": "application/json"
-    }
-
-
-@pytest.fixture
-def new_project_id(headers):
-    """
-    Шаг 3: получаем и сохраняем в поле последнее id
-    """
-    resp = requests.get(f"{BASE_URL}/projects", headers=headers)
-    resp.raise_for_status()
-    new_comp_id = resp.json()["content"][-1]["id"]
-    return new_comp_id
-
-
-def test_change_project(headers, new_project_id):
+def test_get_project(headers, new_project_id):
     url = f"{BASE_URL}/projects/{new_project_id}"
     body = {"title": "modified in test title"}
 
-    resp = requests.put(url, headers=headers, json=body)
+    resp = requests.get(url, headers=headers, json=body)
     assert resp.status_code == 200, f"Ожидался 200, получил {resp.status_code}"
 
     received_project = resp.json()
